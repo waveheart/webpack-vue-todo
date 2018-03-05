6439d07aa17b5c49841da10645083b6e0efc93f5
waveheart <guoyaxin208@sina.com>
2018-03-05 12:27:01 +0800

    webpack-vue-todo

---
+++ src/app.vue
@@ -0,0 +1,52 @@
+<!-- 
+    **author:gyx
+    **msg:Be happy! Guy!
+ -->
+
+<template>
+  <div id="app">
+    <div id="cover"></div>
+    <Iheader></Iheader>
+    <Todo></Todo>
+    <Ifooter></Ifooter>
+  </div>
+</template>
+
+<script>
+import Iheader from './todo/header.vue'
+import Ifooter from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
+
+export default {
+  data () {
+    return {
+      text: 'abcde'
+    }
+  },
+  components: {
+    Iheader,
+    Ifooter,
+    Todo
+  }
+}
+</script>
+
+<style lang="stylus" scoped="">
+#app{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+}
+#cover{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #999
+    opacity .4
+    z-index -1
+}
+</style>
---
+++ src/todo/header.vue
@@ -0,0 +1,32 @@
+<!-- 
+    **author:gyx
+    **msg:Be happy! Guy!
+ -->
+
+<template>
+  <header class="main-header">
+  	<h1>MY TODO</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped="">
+  .main-header{
+      text-align center
+      h1{
+          font-size 100px
+          color rgba(175,47,47,0.4)
+          font-weight 100
+          margin 20px
+      }
+  }
+</style>
